4a620f5be1300e9ade96c52884c461bc3c48af02
midhuna-21 <krishnamidhuna850@gmail.com>
2025-08-11 10:35:48 +0530

    detail page 01

--- src/components/DetailFristSection.tsx
+++ src/components/DetailFristSection.tsx
@@ -76,15 +76,10 @@ const DetailFirstSection: React.FC<Props> = ({ data }) => {
             <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
             </svg>
-            {formatDate(data.date)}
+            {data.date}
           </time>
           <span className="mx-3">•</span>
-          <span className="flex items-center">
-            <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-            {Math.ceil(data.description.split(' ').length / 200)} min read
-          </span>
+         
         </div>
 
         {/* Lead paragraph / Short description */}
